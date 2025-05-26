97d4468d2e8a99a6eb2d5db1c10b6fb781233ba3
rameej-97 <rameej177@gmail.com>
2025-05-26 21:05:45 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,8 @@
+a=input('first name: ')
+b=input('second name: ')
+c=a+" "+b
+print("hello "+c+"! welcome to the python world")
+
+
+
+
---
+++ maths.py
@@ -0,0 +1,16 @@
+a=input('first user: ')
+b=input('second user: ')
+a=float(a)
+b=float(b)
+addition=a+b
+substraction=a-b
+multification=a*b
+division=a/b
+print("addition: ", addition)
+print("substraction: ", substraction)
+print("multification: ", multification)
+print("division: ", division)
+
+
+
+
