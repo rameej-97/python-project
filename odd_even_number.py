4ffabb62ba3318b69d7651352811951bae389fa5
rameej-97 <rameej177@gmail.com>
2025-06-07 18:14:22 +0530

    Add files via upload

---
+++ odd_even_number.py
@@ -0,0 +1,15 @@
+#even number
+a=input('enter number: ')
+a=int(a)
+if a % 2 == 0:
+    print('a this is even number')
+else:
+    print('this is odd number')
+#---------------------------------
+#odd number
+a=input('enter number: ')
+a=int(a)
+if a % 2 == 1:
+    print('a this is odd number')
+else:
+    print('this is even number')
